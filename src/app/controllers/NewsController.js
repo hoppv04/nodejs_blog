ba893e56a19180b1e37be0e5ac262cdd345e87bf
hoppv04 <phamhopxd1@gmail.com>
2023-06-24 09:21:15 +0700

    [MVC] Routes & Controllers

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,13 @@
+class NewsController {
+  // [GET] /news
+  index(req, res) {
+    res.render("news");
+  }
+
+  // [GET] /news/:slug
+  show(req, res) {
+    res.send('Hello World');
+  }
+}
+
+module.exports = new NewsController();
