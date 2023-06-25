d73b528b0d24c1f13cb518d07f138d521f4ba054
hoppv04 <phamhopxd1@gmail.com>
2023-06-25 12:53:35 +0700

    Prettier - Code formatter

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,13 +1,13 @@
 class NewsController {
-  // [GET] /news
-  index(req, res) {
-    res.render("news");
-  }
+    // [GET] /news
+    index(req, res) {
+        res.render('news');
+    }
 
-  // [GET] /news/:slug
-  show(req, res) {
-    res.send('Hello World');
-  }
+    // [GET] /news/:slug
+    show(req, res) {
+        res.send('Hello World');
+    }
 }
 
 module.exports = new NewsController();
