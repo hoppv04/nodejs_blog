5f5d779545a036a4f3578747211bde8ab15fd6ec
hoppv04 <phamhopxd1@gmail.com>
2023-06-26 19:43:47 +0700

    [MVC] Model

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,17 @@
+const Course = require('../models/Course');
+
 class SiteController {
     // [GET] /
     index(req, res) {
-        res.render('home');
+        Course.find({})
+            .then((courses) => {
+                res.json(courses);
+            })
+            .catch((err) => {
+                res.status(400).json({ error: 'Error' });
+            });
+
+        // res.render('home');
     }
 
     // [GET] /search
--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const { engine } = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+
+const route = require('./routes');
+const db = require('./config/db');
+
+// Connect to DB
+db.connect();
+
 const app = express();
 const port = 3000;
 
-const route = require("./routes");
-
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
 
@@ -21,17 +26,17 @@ app.use(express.json());
 
 // Template engine
 app.engine(
-  "hbs",
-  engine({
-    extname: ".hbs",
-  })
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
