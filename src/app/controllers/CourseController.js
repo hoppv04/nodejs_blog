b17847ea64f07794763ee2646f3a3b39b0897d86
hoppv04 <phamhopxd1@gmail.com>
2023-06-30 16:43:50 +0700

    [CRUD] Create new course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -12,6 +12,22 @@ class CourseController {
             })
             .catch(next);
     }
+
+    // [GET] /courses/create
+    create(req, res, next) {
+        res.render('courses/create');
+    }
+
+    // [POST] /courses/store
+    store(req, res, next) {
+        const formData = req.body;
+        formData.image = `https://files.fullstack.edu.vn/f8-prod/courses/2.png`;
+        const course = new Course(formData);
+        course
+            .save()
+            .then(() => res.redirect('/'))
+            .catch((error) => {});
+    }
 }
 
 module.exports = new CourseController();
